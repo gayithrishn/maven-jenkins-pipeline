c5bc1069e3daacf4e00233eb0ec7ea845a85e2ae
gayithrishn <gayithrishn@gmail.com>
2025-03-30 16:15:13 +0530

    final updated code

--- src/main/java/com/mvit/App.java
+++ src/main/java/com/mvit/App.java
@@ -11,5 +11,10 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("progran executed successfully");
         System.out.println("this is my first jenkins CI pipeline");
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("is is:" + i);
+            System.out.println("add webhook successfully");
+        }
     }
 }
